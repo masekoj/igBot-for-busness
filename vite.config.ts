52a92fb85f306dec2c4f3f95566e1db6037780d1
masekoj <132418845+masekoj@users.noreply.github.com>
2025-12-19 09:04:35 +0200

    Set base path for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/igBot-for-busness/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
